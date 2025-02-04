303b1b168cb9228400dca8f18527f45999f14a66
abhay chauhan <chauhanabhay.chauhan2@gmail.com>
2025-02-05 00:10:01 +0530

    feat: add github oauth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+// Add type safety
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+    redirect() {
+      return "/";
+    },
+  },
+});
