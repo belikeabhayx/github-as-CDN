d925b07b8fbc79697634d86938dd4bcb6b354e53
abhay chauhan <chauhanabhay.chauhan2@gmail.com>
2025-02-05 15:51:59 +0530

    feat: make auth accept githubId and access token

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import GitHub from "next-auth/providers/github";
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string;
+      id?: string;
       name?: string | null;
       email?: string | null;
       image?: string | null;
@@ -21,22 +21,38 @@ export const {
 } = NextAuth({
   providers: [
     GitHub({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_APP_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_APP_CLIENT_ACCESS_TOKEN!,
+      authorization: {
+        params: {
+          scope: "repo read:user user:email",
+        },
+      },
     }),
   ],
   session: { strategy: "jwt" },
   callbacks: {
-    jwt({ token, user }) {
-      console.log("JWT callback:", { token, user });
+    jwt({ token, user, account, profile, session }) {
+      if (profile && profile.id) {
+        token.githubId = profile.id;
+      }
       if (user) {
         token.id = user.id;
       }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      console.log("JWT callback:", token);
       return token;
     },
     session({ session, token }) {
       console.log("Session callback:", { session, token });
-      session.user.id = token.id as string;
+      session.user = {
+        ...session.user,
+        id: token.githubId as string,
+      }
+      session.sessionToken = token.accessToken as string;
+     
       return session;
     },
     redirect() {
