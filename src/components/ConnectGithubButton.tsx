0f12e84b666556ba351dff53dea98e10e711b684
abhay chauhan <chauhanabhay.chauhan2@gmail.com>
2025-02-07 01:36:30 +0530

    fix: fix signin button on navbar

--- src/components/ConnectGithubButton.tsx
+++ src/components/ConnectGithubButton.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link";
-import React, { useContext } from "react";
+import React from "react";
 import { LinkIcon } from "lucide-react";
+import { signIn } from "next-auth/react";
 
 function ConnectGithubButton({ className }: { className?: string }) {
   return (
-    <Link
-      href={`/api/auth/callback/github`}
+    <div
       className={`ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 ${className}`}
     >
-      Connect <LinkIcon className="ml-2 size-5 text-white" aria-hidden="true" />
-    </Link>
+      <button onClick={() => signIn("github")}>Sign in with GitHub</button>{" "}
+      <LinkIcon className="ml-2 size-5 text-white" aria-hidden="true" />
+    </div>
   );
 }
 
