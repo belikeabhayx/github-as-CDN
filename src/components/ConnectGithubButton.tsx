bee91a45a363f3d50a013c825ef5bfb949aa2f6f
abhay chauhan <chauhanabhay.chauhan2@gmail.com>
2025-02-23 01:13:26 +0530

    style: change button color

--- src/components/ConnectGithubButton.tsx
+++ src/components/ConnectGithubButton.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { LinkIcon } from "lucide-react";
 import { signIn } from "next-auth/react";
+import { Button } from "./ui/button";
 
 function ConnectGithubButton({ className }: { className?: string }) {
   return (
     <div
-      className={`ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 ${className}`}
+      className={`ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md  px-4 py-2 text-base font-medium text-white shadow-sm  ${className}`}
     >
-      <button onClick={() => signIn("github")}>Sign in with GitHub</button>{" "}
+      <Button
+        size={"lg"}
+        className="rounded-xl"
+        onClick={() => signIn("github")}
+      >
+        Sign in with GitHub
+      </Button>
       <LinkIcon className="ml-2 size-5 text-white" aria-hidden="true" />
     </div>
   );
